fc6a0717bf4485d268394a0c9270046e7904eb11
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-10-06 03:29:10 +0000

    feat(app) css e typescript #0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 import { emmetHTML } from "emmet-monaco-es";
 
-import './App.css';
+import { html5code, csscode, tscode, markdowncode } from "./SeedWork";
+
+import logoSvg from "./assets/logo.svg";
+import "./App.css";
+
+type tabs = "html" | "css" | "ts" | "markdown";
 
 function App() {
+  const [tab, setTab] = useState<tabs>("html");
+
   function handleEditorDidMount() {
     emmetHTML();
   }
 
-
   return (
-    <div className="App">
-      <Editor
-              height="90vh"
-              theme="dark"
-              language="text/html"
-              editorDidMount={handleEditorDidMount}
-              />
+    <div className="app">
+      <nav>
+        <img src={logoSvg} />
+        <button
+          onClick={() => setTab("html")}
+          className={tab === "html" ? "active" : undefined}
+        >
+          HTML
+        </button>
+        <button
+          onClick={() => setTab("css")}
+          className={tab === "css" ? "active" : undefined}
+        >
+          CSS
+        </button>
+        <button
+          onClick={() => setTab("ts")}
+          className={tab === "ts" ? "active" : undefined}
+        >
+          TS
+        </button>
+        <button
+          onClick={() => setTab("markdown")}
+          className={tab === "markdown" ? "active" : undefined}
+        >
+          MARKDOWN
+        </button>
+      </nav>
+      <section>
+        <div className={["editor", tab === "html" ? "active" : ""].join(" ")}>
+          <Editor
+            height="100vh"
+            width="100vw"
+            theme="dark"
+            language="text/html"
+            value={html5code}
+            editorDidMount={handleEditorDidMount}
+          />
+        </div>
+        <div className={["editor", tab === "css" ? "active" : ""].join(" ")}>
+          <Editor
+            height="100vh"
+            width="100vw"
+            theme="dark"
+            value={csscode}
+            language="text/css"
+          />
+        </div>
+        <div className={["editor", tab === "ts" ? "active" : ""].join(" ")}>
+          <Editor
+            height="100vh"
+            width="100vw"
+            theme="dark"
+            value={tscode}
+            language="typescript"
+          />
+        </div>
+        <div
+          className={["editor", tab === "markdown" ? "active" : ""].join(" ")}
+        >
+          <Editor
+            height="100vh"
+            width="100vw"
+            theme="dark"
+            value={markdowncode}
+            language="markdown"
+          />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/SeedWork.ts
@@ -0,0 +1,23 @@
+export const html5code = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<div id="app"></div>
+</body>
+
+</html>`;
+
+export const csscode = `html, body {
+  margin: 0px;
+  padding: 0px;
+}`;
+
+export const tscode = `const app = document.querySelector('app');`;
+
+export const markdowncode = `# App Project`;
