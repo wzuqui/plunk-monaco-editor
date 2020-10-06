a9c3a93c8e8fd49e169c65319bb744443dd4af83
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-10-06 03:35:29 +0000

    build(frontend) publish #0

---
+++ build/precache-manifest.b110278b5da068f71a7c4c0a52d148af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e7726bed11ad7d44058a4c1512624d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "105b79415eb48d9ea141",
+    "url": "/static/css/main.06d7cf5e.chunk.css"
+  },
+  {
+    "revision": "9c80dd1d7cbe66134b59",
+    "url": "/static/js/2.706dc061.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.706dc061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105b79415eb48d9ea141",
+    "url": "/static/js/main.987040d8.chunk.js"
+  },
+  {
+    "revision": "e43df535e56c922454e5",
+    "url": "/static/js/runtime-main.118d18e0.js"
+  },
+  {
+    "revision": "da8c61606826f35ed0f077ad2d104a57",
+    "url": "/static/media/logo.da8c6160.svg"
+  }
+]);
