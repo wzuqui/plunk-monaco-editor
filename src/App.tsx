7790772e14d21e276a8165d6d673b0d69e4cb29e
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-10-06 01:55:02 +0000

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Editor from '@monaco-editor/react';
+import { emmetHTML } from "emmet-monaco-es";
+
+import './App.css';
+
+function App() {
+  function handleEditorDidMount() {
+    emmetHTML((window as any)['monaco']);
+  }
+
+
+  return (
+    <div className="App">
+      <Editor
+              height="90vh"
+              theme="dark"
+              language="text/html"
+              editorDidMount={handleEditorDidMount}
+              />
+    </div>
+  );
+}
+
+export default App;
